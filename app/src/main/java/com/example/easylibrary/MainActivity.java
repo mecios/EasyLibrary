f185744b5a6a9c37a68ca35fe5f48be7ac07fca5
mecios <914527116@qq.com>
2024-12-02 23:01:08 +0800

    initial commit

---
+++ app/src/main/java/com/example/easylibrary/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.easylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.example.easyutil.DisplayUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int windowsWidth = DisplayUtil.getWindowsWidth(this);
+        int windowsHeight = DisplayUtil.getWindowsHeight(this);
+        Toast.makeText(this, "Height："+windowsHeight+"，Width："+windowsWidth,
+                Toast.LENGTH_SHORT).show();
+    }
+}
