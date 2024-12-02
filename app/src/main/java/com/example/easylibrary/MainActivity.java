e7da51aad5b54ccb5bcb998f5fc32c3bd73ea9db
mecios <914527116@qq.com>
2024-12-03 00:08:33 +0800

    fix function

--- app/src/main/java/com/example/easylibrary/MainActivity.java
+++ app/src/main/java/com/example/easylibrary/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.easylibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.example.easyutil.DisplayUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int windowsWidth = DisplayUtil.getWindowsWidth(this);
-        int windowsHeight = DisplayUtil.getWindowsHeight(this);
-        Toast.makeText(this, "Height：" + windowsHeight + "，Width：" + windowsWidth,
-                Toast.LENGTH_SHORT).show();
     }
 }
